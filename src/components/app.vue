76966510178bce8ef212d1588eebba60dd223ab3
ly102412 <15102239007@163.com>
2017-09-09 17:00:20 +0800

    vue 项目

---
+++ src/components/app.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <header class="site-header jumbotron">
+      <div class="container">
+        <div class="row">
+          <div class="col-xs-12">
+            <h1>请发表对React的评论</h1>
+          </div>
+        </div>
+      </div>
+    </header>
+    <div class="container">
+      <add :addComment="addComment"></add>
+      <list :comments="comments" :deleteComment="deleteComment"></list>
+    </div>
+  </div>
+</template>
+<script>
+  import add from "./add.vue"
+  import list from "./list.vue"
+
+  export default {
+
+    data () {
+      return {
+        comments: [
+          {name: 'llc',content: 'Vue is very good'},
+          {name: 'zxf',content: 'Vue is very very good'}
+        ]
+      }
+    },
+
+    methods: {
+      addComment (comment) {
+        this.comments.unshift(comment);
+      },
+      deleteComment (index) {
+        this.comments.splice(index,1);
+      }
+    },
+
+    components: {
+      add,
+      list
+    }
+  }
+</script>
+<style>
+</style>
---
+++ src/components/list.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="col-md-8">
+    <h3 class="reply">评论回复：</h3>
+    <h2 style='display: none'>暂无评论，点击左侧添加评论！！！</h2>
+    <ul class="list-group">
+      <item v-for="(comment,index) in comments" :comment="comment" :key="index" :index="index" :deleteComment="deleteComment"></item>
+    </ul>
+  </div>
+</template>
+<script>
+  import item from "./item.vue";
+
+  export default {
+    props: ["comments","deleteComment"],
+    components: {
+      item
+    }
+  };
+</script>
+<style>
+  .reply {
+    margin-top: 0px;
+  }
+</style>
